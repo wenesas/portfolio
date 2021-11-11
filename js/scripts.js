c14ba26eef37893c369a8131854d3fec5c0dcfd3
kotons <sarveshdhar499@gmail.com>
2021-11-12 02:58:25 +0530

    dev-projet gallery added

--- js/scripts.js
+++ js/scripts.js
@@ -67,3 +67,48 @@
   
   })(jQuery); // End of use strict
   
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  captionText.style.fontFamily = "33px, SB Heading";
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+function add_screen_notice(){
+  if (screen.width < 576){
+    var add_notice = document.getElementById("big_screen_notice");
+    add_notice.innerHTML = "Open in desktop for better view";
+  }
+}
